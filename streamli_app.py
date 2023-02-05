bf17d34e4c218141f858ab3e0c9842aa0a6434dd
Shiva Sahu <63371513+jacktravenk123@users.noreply.github.com>
2023-02-05 18:32:51 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -2,6 +2,7 @@ import streamlit
 import requests
 import pandas
 import snowflake.connector
+from urllib import URLError
 
 streamlit.title('My cafe')
 streamlit.header('Breakfast Menu')
@@ -28,4 +29,4 @@ streamlit.dataframe(my_data_row)
 fruit_add = streamlit.text_input('What fruit would you like add?','Ex: Kiwi')
 streamlit.write("Thanks for adding :", fruit_add)
 
-my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values (" + fruit_add + ")")
+my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('"fruit_add"')")
