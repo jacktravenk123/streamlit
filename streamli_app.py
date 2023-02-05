421103eea84c0835a68c58aa706d27707bef9d74
Shiva Sahu <63371513+jacktravenk123@users.noreply.github.com>
2023-02-05 16:13:56 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -1,3 +1,11 @@
 import streamlit
+import requests
 
 streamlit.title('My cafe')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oatmeal')
+streamlit.text('Smoothie')
+streamlit.text('Boiled Eggs')
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response)
