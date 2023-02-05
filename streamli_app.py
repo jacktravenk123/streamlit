537c3d21d3a764aec4ac9df3cf00dd73c02b07c8
Shiva Sahu <63371513+jacktravenk123@users.noreply.github.com>
2023-02-05 16:17:35 +0530

    Update streamli_app.py

--- streamli_app.py
+++ streamli_app.py
@@ -7,5 +7,8 @@ streamlit.text('Oatmeal')
 streamlit.text('Smoothie')
 streamlit.text('Boiled Eggs')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+streamlit.dataframe(fruityvice_normalized)
